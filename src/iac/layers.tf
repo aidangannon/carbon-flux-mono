89388ea6935f93fd8bb13de3baec7ece65ff52bd
aidan <aidanwilliamgannon@gmail.com>
2025-09-04 22:07:55 +0100

    Update lambda layers

--- src/iac/layers.tf
+++ src/iac/layers.tf
@@ -1,11 +1,11 @@
 resource "aws_lambda_layer_version" "core_layer" {
   filename            = local.file_name
-  layer_name          = "core-lib"
+  layer_name          = "common"
   compatible_runtimes = [var.python_runtime]
 
   source_code_hash = filebase64sha256(local.file_name)
 }
 
 locals {
-  file_name = "../../dist/lambda-layers.core/lambda-layer.zip"
+  file_name = "../../dist/lambda-layers.common/lambda-layer.zip"
 }
