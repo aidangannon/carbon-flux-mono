3801188c05598adf8b46c1bf9598995497400ec5
aidan <aidanwilliamgannon@gmail.com>
2025-09-09 20:56:33 +0100

    Move boto3 resource

--- src/fluxter_scrape/services.py
+++ src/fluxter_scrape/services.py
@@ -6,15 +6,13 @@ import boto3
 from src.common.logging import Logger
 
 
-dynamodb = boto3.resource('dynamodb', region_name='eu-west-2')
-
-
 @dataclass(frozen=True, slots=True)
 class Dependency:
     logger: Logger
 
     def __call__(self, item_id: str) -> Optional[dict]:
         self.logger.info("Logging from inner service")
+        dynamodb = boto3.resource('dynamodb', region_name='eu-west-2')
         response = dynamodb.Table('fluxter-db').get_item(Key={
             'partition_key': 'item',
             'id': item_id
