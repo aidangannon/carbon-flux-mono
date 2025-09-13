326fee738084697a54d7388cbfa2a0ef085a36f8
aidan <aidanwilliamgannon@gmail.com>
2025-09-13 10:27:55 +0100

    Remove pandas layer arn for now

--- src/iac/fluxter/scheduler.tf
+++ src/iac/fluxter/scheduler.tf
@@ -7,8 +7,7 @@ module "lambda_function" {
   runtime       = var.python_runtime
 
   layers = [
-    var.core_layer_arn,
-    var.pandas_layer_arn
+    var.core_layer_arn
   ]
 
   create_package         = false
