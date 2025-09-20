88c269f22426dfd953a10324a7a9f5dd7dc30e62
aidan <aidanwilliamgannon@gmail.com>
2025-09-20 22:09:34 +0100

    Explicit context passing here is fine

--- tests/flux_tracking_service/features/get_item_feature/get_item_feature.py
+++ tests/flux_tracking_service/features/get_item_feature/get_item_feature.py
@@ -9,31 +9,31 @@ from tests.flux_tracking_service.infrastructure.common_steps.log_steps import th
 
 @scenario
 def test_1(get_item_feature):
-    context = get_item_feature
-    context.runner \
-        .given(data_exists_in_the_db(context)) \
-        .when(lambda_is_called_with_data_id(context)) \
-        .then(lambda_response_should_equal_data(context)) \
-        .and_also(there_should_be_an_LEVEL_log_with_message_MESSAGE(context, "Logging from service", "INFO")) \
+    ctx = get_item_feature
+    ctx.runner \
+        .given(data_exists_in_the_db(ctx)) \
+        .when(lambda_is_called_with_data_id(ctx)) \
+        .then(lambda_response_should_equal_data(ctx)) \
+        .and_also(there_should_be_an_LEVEL_log_with_message_MESSAGE(ctx, "Logging from service", "INFO")) \
         .run()
 
 @scenario
 def test_2(get_item_feature):
-    context = get_item_feature
-    context.runner \
+    ctx = get_item_feature
+    ctx.runner \
         .given(no_data_exists()) \
-        .when(lambda_is_called_with_data_id(context)) \
-        .then(lambda_response_should_be_empty(context)) \
-        .and_also(lambda_response_should_be_empty(context)) \
+        .when(lambda_is_called_with_data_id(ctx)) \
+        .then(lambda_response_should_be_empty(ctx)) \
+        .and_also(lambda_response_should_be_empty(ctx)) \
         .run()
 
 @scenario
 def test_another_blady_test(get_item_feature):
-    context = get_item_feature
-    context.runner \
-        .given(data_exists_in_the_db_with_other_partition_PARTITION_and_number_NUMBER_and_person_PERSON(context, "123", 2, {"name": "", "blah": "new", "attributes": {}})) \
-        .when(lambda_is_called_with_data_id(context)) \
-        .then(lambda_response_should_be_empty(context)) \
-        .and_also(lambda_response_should_be_empty(context)) \
-        .and_also(needless_step_to_test_our_args(context, 2, "123")) \
+    ctx = get_item_feature
+    ctx.runner \
+        .given(data_exists_in_the_db_with_other_partition_PARTITION_and_number_NUMBER_and_person_PERSON(ctx, "123", 2, {"name": "", "blah": "new", "attributes": {}})) \
+        .when(lambda_is_called_with_data_id(ctx)) \
+        .then(lambda_response_should_be_empty(ctx)) \
+        .and_also(lambda_response_should_be_empty(ctx)) \
+        .and_also(needless_step_to_test_our_args(ctx, 2, "123")) \
         .run()
