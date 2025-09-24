d545442d68584b7ac087a0a37b410a76b67b7f3f
aidan <aidanwilliamgannon@gmail.com>
2025-09-24 21:12:54 +0100

    Add test when a submission is deleted for site

--- tests/flux_tracking_service/features/retrieve_flux_submissions_feature/retrieve_flux_submissions_feature.py
+++ tests/flux_tracking_service/features/retrieve_flux_submissions_feature/retrieve_flux_submissions_feature.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 
 from tests import scenario
 from tests.flux_tracking_service.features.retrieve_flux_submissions_feature.retrieve_flux_submissions_feature_steps import \
@@ -30,12 +30,27 @@ def test_when_no_submissions_are_available_for_site(retrieve_flux_submissions_fe
         .run_all_steps()
 
 @scenario
-def test_when_a_submission_is_available_for_site_that_has_already_been_processed(retrieve_flux_submissions_feature):
+def test_when_a_submission_is_has_already_been_processed_for_the_site(retrieve_flux_submissions_feature):
     submission_time = datetime.now(tz=timezone.utc)
 
     ctx = retrieve_flux_submissions_feature
     ctx.runner \
         .given(a_tracked_site_is_added_with_last_fetched_LAST_FETCHED(ctx, last_fetched=submission_time)) \
+        .and_also(icos_api_is_configured_with_station_STATION_ID_to_return_submission_OBJECT_ID(ctx.station, ctx.submission, submission_time, ctx.requests_mock)) \
+        .when(lambda_is_invoked(ctx)) \
+        .then(result_is_empty(ctx)) \
+        .and_also(there_should_be_a_log_with_severity_LEVEL_and_message_MESSAGE(f"no new submissions for {ctx.station}", "WARNING", ctx.container)) \
+        .run_all_steps()
+
+@scenario
+def test_when_a_submission_is_deleted_for_site(retrieve_flux_submissions_feature):
+    submission_time = datetime.now(tz=timezone.utc) - timedelta(days=5)
+    last_fetched_for_site = datetime.now(tz=timezone.utc)
+
+
+    ctx = retrieve_flux_submissions_feature
+    ctx.runner \
+        .given(a_tracked_site_is_added_with_last_fetched_LAST_FETCHED(ctx, last_fetched=last_fetched_for_site)) \
         .and_also(icos_api_is_configured_with_station_STATION_ID_to_return_submission_OBJECT_ID(ctx.station, ctx.submission, submission_time, ctx.requests_mock)) \
         .when(lambda_is_invoked(ctx)) \
         .then(result_is_empty(ctx)) \
