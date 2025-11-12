24ea546c856c171f9d2e85320e0e11c6b93af203
aidan <aidanwilliamgannon@gmail.com>
2025-11-12 20:27:43 +0000

    Change github actions pipeline to match deployment structure

--- src/carbon_tracking_service/deploy/scheduler.tf
+++ src/carbon_tracking_service/deploy/scheduler.tf
@@ -13,7 +13,7 @@ module "lambda_function" {
   ]
 
   create_package         = false
-  local_existing_package = "../../dist/src.carbon_tracking_service.entry_points/get_latest_submissions_lambda.zip"
+  local_existing_package = "../../../dist/src.carbon_tracking_service.entry_points/get_latest_submissions_lambda.zip"
 
   attach_policy_statements = true
   attach_cloudwatch_logs_policy = true
