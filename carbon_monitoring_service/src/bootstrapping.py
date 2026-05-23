817939f30642f83cb37832bac77de3a4317b692b
aidangannon <aidanwilliamgannon@gmail.com>
2026-05-23 16:59:50 +0100

    Add autocomplete

--- carbon_monitoring_service/src/bootstrapping.py
+++ carbon_monitoring_service/src/bootstrapping.py
@@ -5,7 +5,6 @@ from carbon_monitoring_service.src.application import ports
 
 __all__ = ["configure_adapters"]
 
-
 def configure_adapters():
     ports.monitored_site_repository = dynamo.DynamoMonitoredSiteRepository()
     ports.flux_client = icos.IcosFluxClient()
